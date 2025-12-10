3635ae60c5493418a86b1691958a507a8f7da0e0
Nikolai Hecker <nikolai.hecker@ukdri.ac.uk>
2025-12-10 12:18:39 +0000

    Improved memory usage for ADD_OBS_OBM

--- modules/local/adata/add_obs_obsm/templates/add_obs_obsm.py
+++ modules/local/adata/add_obs_obsm/templates/add_obs_obsm.py
@@ -8,25 +8,33 @@ import pandas as pd
 import numpy as np
 import yaml
 
+# TODO:  extract obs, obsm, obsnanes for adata_add, del adata_add, get index for subsetting from shared genes
 adata = ad.read_h5ad("${adata_base}")
 adata_add = ad.read_h5ad("${adata_add}")
 prefix = "${prefix}"
 
+# get obs, obsm
+add_obs = adata_add.obs.copy()
+add_obsm = adata_add.obsm.copy()
+add_obsnames = [str(bc) for bc in adata_add.obs_names ]
+del adata_add
 
 # subset to overlapping cells
-shared_bcs = [bc for bc in adata.obs_names if bc in adata_add.obs_names ]
+shared_bcs = [bc for bc in adata.obs_names if bc in add_obsnames ]
 
+# subset to shared and add to base adata
 adata = adata[shared_bcs].copy()
-adata_add = adata_add[shared_bcs].copy()
 
+dict_idx = {bc:idx for idx, bc in enumerate(add_obsnames) }
+order_add = [ dict_idx[bc] for bc in shared_bcs ]
+add_obs = add_obs.iloc[order_add, :].copy()
 
-for col in adata_add.obs:
+for col in add_obs:
     if col not in adata.obs:
-        adata.obs[col] = adata_add.obs[col].copy()
-
-for emb in adata_add.obsm:
+        adata.obs[col] = add_obs[col].copy()
+for emb in add_obsm:
     if emb not in adata.obsm:
-        adata.obsm[emb] = adata_add.obsm[emb].copy()
+        adata.obsm[emb] = add_obsm[emb][order_add, :].copy()
 
 
 adata.write_h5ad(f"{prefix}.h5ad")
